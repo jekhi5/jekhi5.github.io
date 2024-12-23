b4ab265fd8a3a135d23416a61ce8604a39aeeb46
Jacob Kline <jacobk513@gmail.com>
2024-12-22 23:17:05 -0600

    update tags for Kanbas

--- src/projectsData.ts
+++ src/projectsData.ts
@@ -20,7 +20,7 @@ const projects: Project[] = [
     description: 'A simulation of the teaching and learning software, Canvas',
     image: 'KanbasImage.png',
     imageAltText: 'List of courses on the Kanbas Courses page',
-    tags: ['React', 'TypeScript', 'MongoDB', 'Firebase', 'Express', 'Node'],
+    tags: ['React', 'TypeScript', 'MongoDB', 'Express', 'Node'],
     url: 'https://kanbas-project.netlify.app/#/Labs/Lab1',
     frontEndUrl: 'https://github.com/jekhi5/Kanbas',
     backEndUrl: 'https://github.com/jekhi5/Kanbas-backend',

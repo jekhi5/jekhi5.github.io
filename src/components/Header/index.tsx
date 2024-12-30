480e1f87eefdc192a75c33252196e100148f3864
Jacob Kline <jacobk513@gmail.com>
2024-12-30 12:21:35 -0500

    make header stick

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,7 +3,7 @@ import './index.css';
 
 const Header = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light shadow-sm sticky-top">
+    <nav className="navbar navbar-expand-lg navbar-light bg-light shadow-sm">
       <div className="container">
         <NavLink to="/portfolio/home" className="navbar-brand fs-4 fw-bold">
           Jacob Kline

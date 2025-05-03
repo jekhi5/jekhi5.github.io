5ed41c59a5044ee850c38dae2e6f7c165265ea30
Jacob Kline <jacobk513@gmail.com>
2025-05-03 16:11:34 -0400

    contact form connected to backend

--- src/components/ContactForm/index.tsx
+++ src/components/ContactForm/index.tsx
@@ -1,56 +1,79 @@
 import { useState } from 'react';
-import emailjs from '@emailjs/browser';
 import './index.css';
 import DetailButtons from 'components/Home/DetailButtons';
 
+const apiBase = 'https://jekhi5.netlify.app';
+
 export default function ContactForm() {
+  const inquiryReasons: { [key: string]: string } = {
+    github: 'GitHub Project Access Request',
+    networking: 'Networking',
+    personal: 'Personal',
+    other: 'Other',
+  };
+
+  const defaultInquiryReason = 'github';
+
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [stateMessage, setStateMessage] = useState('');
-  // const [inquiryReason, setInquiryReason] = useState('');
+  const [formName, setFormName] = useState('');
+  const [formEmail, setFormEmail] = useState('');
+  const [formMessage, setFormMessage] = useState('');
+  const [formSubject, setFormSubject] = useState('');
+  const [formInquiryReason, setFormInquiryReason] = useState(
+    inquiryReasons[defaultInquiryReason]
+  );
 
-  const sendEmail = (e: any) => {
+  const sendEmail = async (e: any) => {
     e.persist();
     e.preventDefault();
     setIsSubmitting(true);
-    emailjs
-      .sendForm(
-        process.env.REACT_APP_SERVICE_ID ?? '',
-        process.env.REACT_APP_TEMPLATE_ID ?? '',
-        e.target,
-        process.env.REACT_APP_PUBLIC_KEY
-      )
-      .then(
-        (_) => {
-          setStateMessage('Message sent!');
-          setIsSubmitting(false);
-          setTimeout(() => {
-            setStateMessage('');
-          }, 5000);
-          // Reset form
-          e.target.reset();
-        },
-        (_) => {
-          setStateMessage('Something went wrong, please try again later');
-          setIsSubmitting(false);
-          setTimeout(() => {
-            setStateMessage('');
-          }, 5000);
-        }
-      );
+
+    const newMessage = `Inquiry Reason: ${formInquiryReason}\n\n ${formMessage}`;
+
+    const formData = {
+      name: formName,
+      email: formEmail,
+      message: newMessage,
+      title: formSubject,
+    };
+
+    const res = await fetch(`${apiBase}/api/form_submit`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(formData),
+    });
+
+    if (res.ok) {
+      setStateMessage('Message sent!');
+      setIsSubmitting(false);
+      setTimeout(() => {
+        setStateMessage('');
+      }, 5000);
+      // Reset form
+      e.target.reset();
+    } else {
+      const responseText = await res.text();
+      setStateMessage(responseText);
+      setIsSubmitting(false);
+      setTimeout(() => {
+        setStateMessage('');
+      }, 5000);
+    }
   };
 
-  // function updateInquiry(e: React.FormEvent<HTMLOptionElement>) {
-  //   setInquiryReason(e.currentTarget.value);
-  // }
+  function updateInquiry(key: string) {
+    setFormInquiryReason(inquiryReasons[key]);
+  }
 
   return (
     <div className="vh-100">
       <div className="contact-form">
         <h2>Contact Me</h2>
-        <text>
+        <h6>
           Let's get in touch! If you'd like to learn more about my experience,
           please feel free to check out my LinkedIn, GitHub, and Resume below:
-        </text>
+        </h6>
         <DetailButtons />
         <form onSubmit={sendEmail} className="">
           <input
@@ -58,6 +81,7 @@ export default function ContactForm() {
             name="name"
             className="name"
             placeholder="Name*"
+            onChange={(e) => setFormName(e.currentTarget.value)}
             required
           />
           <input
@@ -65,34 +89,33 @@ export default function ContactForm() {
             name="email"
             className="email"
             placeholder="Email*"
+            onChange={(e) => setFormEmail(e.currentTarget.value)}
             required
           />
-          {/* TODO: Continue implementation of this functionality: */}
-          {/* <label>Reason for inquiry:</label>
-          <select required>
-            <option value="github" onChange={updateInquiry}>
-              GitHub Project Access Request
-            </option>
-            <option value="networking" onChange={updateInquiry}>
-              Networking
-            </option>
-            <option value="personal" onChange={updateInquiry}>
-              Personal
-            </option>
-            <option value="other" onChange={updateInquiry}>
-              Other
-            </option>
-          </select> */}
+          <label>Reason for inquiry:</label>
+          <select
+            required
+            defaultValue={defaultInquiryReason}
+            onChange={(e) => updateInquiry(e.currentTarget.value)}
+          >
+            {Object.keys(inquiryReasons).map((key) => (
+              <option value={key} key={key}>
+                {inquiryReasons[key]}
+              </option>
+            ))}
+          </select>
           <input
             type="text"
             name="title"
             className="title"
             placeholder="Subject"
+            onChange={(e) => setFormSubject(e.currentTarget.value)}
           />
           <textarea
             name="message"
             className="message"
             placeholder="Message*"
+            onChange={(e) => setFormMessage(e.currentTarget.value)}
             required
           />
           <input type="submit" value="Send" disabled={isSubmitting} />

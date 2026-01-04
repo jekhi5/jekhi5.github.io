357192d8bbfc3002300373ba47cfafeabb9fc9ea
Jacob Kline <jacobk513@gmail.com>
2026-01-04 10:43:53 -0500

    add blog post for aoc 2025-4

--- src/components/Blog/BlogPosts/index.ts
+++ src/components/Blog/BlogPosts/index.ts
@@ -4,6 +4,8 @@ import { AdventOfCode2025Posts } from './AdventOfCode';
 const allPosts: BlogPost[] = [...AdventOfCode2025Posts];
 
 // Sort posts by date (newest first)
-export const posts = allPosts.sort(
-    (a, b) => b.date.getTime() - a.date.getTime(),
+export const posts = allPosts.sort((a, b) =>
+    b.date.getTime() === a.date.getTime()
+        ? b.title.localeCompare(a.title)
+        : b.date.getTime() - a.date.getTime(),
 );
